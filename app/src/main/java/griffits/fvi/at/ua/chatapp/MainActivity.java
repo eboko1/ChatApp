b3df48f5e6e9a4296fcce05c169c290f6e4a024c
Victoria Frantsuh <eboko1@gmail.com>
2017-07-27 15:07:17 +0300

    add FirebaseUI for Authentication

--- app/src/main/java/griffits/fvi/at/ua/chatapp/MainActivity.java
+++ app/src/main/java/griffits/fvi/at/ua/chatapp/MainActivity.java
@@ -1,5 +1,6 @@
 package griffits.fvi.at.ua.chatapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +10,26 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.auth.AuthUI;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 1;
     //UI
     private ListView mMessageListView;
     private EditText mMessageEditText;
-    private ImageButton mSendButton, mPhotoPickenButton;
+    private ImageButton mSendButton, mPhotoPickerButton;
     private String mUsername;
     private MessageAdapter mMessageAdapter;
 
@@ -32,19 +38,31 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabaseReference;
     private ChildEventListener mChildEventListener;
 
+    //  FirebaseUI for Authentication
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
 
+        // image PickerButton shows an img picker to upload a image for message
+        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
         // initialize Firebase components
         mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference().child("message");
 
-        // create Listener for Firebase
 
+        // create Listener for Firebase
         if (mChildEventListener == null){
             mChildEventListener = new ChildEventListener() {
                 @Override
@@ -73,9 +91,31 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             };
-
             mDatabaseReference.addChildEventListener(mChildEventListener);
-
+            mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if(user != null){
+                        //user sing in
+                        Toast.makeText(MainActivity.this, "Welcome!!!", Toast.LENGTH_LONG).show();
+                    } else {
+                        // user sing out
+                        startActivityForResult(
+                                AuthUI.getInstance()
+                                        .createSignInIntentBuilder()
+                                        .setIsSmartLockEnabled(false)
+                                        .setAvailableProviders(
+                                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                       // new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
+                                                        new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
+                                        .build(),
+                                RC_SIGN_IN);
+                    }
+                }
+            };
         }
 
         List<Message> messages = new ArrayList<>();
@@ -88,12 +128,12 @@ public class MainActivity extends AppCompatActivity {
         mMessageListView = (ListView)findViewById(R.id.messageListView);
         mMessageEditText = (EditText) findViewById(R.id.mesageEditText);
         mSendButton = (ImageButton)findViewById(R.id.sendImageButton);
-        mPhotoPickenButton = (ImageButton)findViewById(R.id.photoPickerImageButton);
+        mPhotoPickerButton = (ImageButton)findViewById(R.id.photoPickerImageButton);
     }
 
     // send button sends a message
     public void sendMessage(View view){
-        Message message = new Message(mMessageEditText.getText().toString(),null,null);
+        Message message = new Message(mMessageEditText.getText().toString(), mUsername, null);
 
         if (mMessageEditText.getText().length() > 0) {
             mDatabaseReference.push().setValue(message);
@@ -104,8 +144,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
 
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
 
     @Override
     protected void onDestroy() {
